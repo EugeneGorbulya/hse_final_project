33353b8605d8e52d70727c729f6ab3d2b041b2d7
EugeneGorbulya <57588872+EugeneGorbulya@users.noreply.github.com>
2023-12-11 18:42:36 +0300

    Update run.py

--- streamlit/run.py
+++ streamlit/run.py
@@ -39,14 +39,14 @@ L: more than 250 employees (large)""")
     st.subheader("Some Graphics")
     choose = st.selectbox("Graphics: ", ['Map', 'Salary in dollars depends on the position', 'Comparison of EU and USA', 'Count of jobs per year'])
     if choose == 'Map':
-        img = Image.open("hse_final_project/static/map.png")
+        img = Image.open("map.png")
         st.image(img)
     if choose == 'Salary in dollars depends on the position':
-        img = Image.open("hse_final_project/static/comp_salary.png")
+        img = Image.open("comp_salary.png")
         st.image(img)
     if choose == 'Comparison of EU and USA':
-        img = Image.open("hse_final_project/static/comparison.png")
+        img = Image.open("comparison.png")
         st.image(img)
     if choose == 'Count of jobs per year':
-        img = Image.open("hse_final_project/static/count_of_jobs.png")
+        img = Image.open("count_of_jobs.png")
         st.image(img)
